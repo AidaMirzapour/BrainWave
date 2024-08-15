e8a9e979c03ca13c3e82a1847f64b9f9b0a2c599
Aida <113546067+AidaMirzapour@users.noreply.github.com>
2024-08-15 11:58:40 +0330

    add BrainWave Project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Button from "./components/Button";
+import Collaboration from "./components/Collaboration";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
