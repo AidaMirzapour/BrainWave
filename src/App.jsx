ec5b69b2de57b2d3e03e91bad919c2b4a8476bdf
Aida <113546067+AidaMirzapour@users.noreply.github.com>
2024-08-22 14:39:24 +0330

    create Services component and add first service- 1403/6/1 - 14:39

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Collaboration />
       </div>
       <ButtonGradient />
+      <Services />
     </>
   );
 };
