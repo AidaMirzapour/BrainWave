a46b7cca160b0f5293d3c3261a5d2a0e6eb192e2
Aida <113546067+AidaMirzapour@users.noreply.github.com>
2024-08-23 13:41:26 +0330

    edit pricingList style - 1403/6/2 - 13:41

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -8,17 +8,17 @@ const PricingList = () => {
       {pricing.map((item) => (
         <div
           key={item.id}
-          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-6"
+          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-10 even:!pb-16 even:border-n-3 odd:py-10  [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-6"
         >
           <h4 className="h4 mb-4">{item.title}</h4>
           <p className="body-2 text-n-1/50 min-h-[4rem] mb-3">
             {item.description}
           </p>
-          <div className="flex gap-2 items-center h-[5.5rem] mb-6">
+          <div className="flex gap-2 items-center justify-center h-[5.5rem] mb-6">
             {item.price && (
               <>
                 <div className="h3">$</div>
-                <div className="text-[5rem] leading-none font-bold">
+                <div className="text-[3.5rem] leading-none font-bold">
                   {item.price}
                 </div>
               </>
